012b54d1d85bbecbcb2152accac8578b07e28000
MalakasP <pvlsmlks@gmail.com>
2023-10-29 18:27:42 +0200

    add API and WEB classes for routes

---
+++ app/routes/WEB.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Routes;
+
+use Slim\App as SlimApp;
+
+class WEB
+{
+
+    public static function setRoutes(SlimApp $app): SlimApp
+    {
+        // TODO: implement later if needed
+        return $app;
+    }
+}
